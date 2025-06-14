e1e523de17aff41b32dd60f4fd4397858158d698
preronagit <karprerona6@gmail.com>
2025-06-14 20:20:31 +0530

    server.js updated for deplpying in render

--- server/server.js
+++ server/server.js
@@ -49,15 +49,15 @@ app.use("/api/auth",userRouter);
 app.use("/api/messages",messageRouter);
 
 
+ const startServer = async () => {
   await connectDB();
 
-  if(process.env.NODE_ENV !=="production")
-  {
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () =>
-      console.log("Server is running on PORT: " + PORT)
-    );
-  }
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+};
+
+startServer();
+  
 
   //Export server for vercel
   export default server;
