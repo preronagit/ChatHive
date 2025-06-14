7ed862be3d2c0fa20379ed64cfd16a8e938e19ee
preronagit <karprerona6@gmail.com>
2025-06-14 11:58:29 +0530

    server.js updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
     <div
-      className="bg-[url('./src/assets/bgl_image.svg')] 
+      className="bg-[url('/bgl_image.svg')] 
     bg-contain"
     >
       <Toaster />
